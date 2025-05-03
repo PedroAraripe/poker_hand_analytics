a6085dff5f8b1ab44f03a912ca80e5d6bde31381
Lucas Araripe <pedro.lucx@gmail.com>
2025-05-03 00:30:04 -0300

    fix  (Cors) : Yeah... we all knew this was going to happen

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
 from routes.hand_analysis import router as hand_analysis_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+# Defina aqui os domínios que podem acessar sua API
+origins = [
+    "http://localhost",
+    "http://localhost:3000",  # frontend local
+    "https://meusite.com",    # domínio em produção
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Registra as rotas do módulo routes/hand_analysis.py
 app.include_router(hand_analysis_router)
